608b698f0c7fdb6861a29feb51e2fb0022cc6983
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-02 23:08:05 +0530

    AWS S3

--- Utils/AWS/S3.js
+++ Utils/AWS/S3.js
@@ -1,3 +1,15 @@
 import AWS from "aws-sdk";
 
-const
+const s3Bucket = new AWS.S3({
+    accessKeyId: "",
+    secretAccessKey: "",
+    region: "ap-south-1"
+});
+
+export const s3Upload = (options) => {
+    return new Promise((resolve, reject) => 
+    s3Bucket.upload(options, (error, data) => {
+        
+    })
+    );
+};
