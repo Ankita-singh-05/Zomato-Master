6112d566ca826d3bde7ba36fa89250ff94c7a6c8
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-02 22:57:04 +0530

    AWS S3

--- API/Image/image.js
+++ API/Image/image.js
@@ -1,9 +1,14 @@
 import express from "express";
 import AWS from "aws-sdk";
+import multer from "multer"; 
 
 import { ImageModel } from "../../Database/allModels";
 
 const Router = express.Router();
+
+//Multer CONFIG
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 /* 
 Route      /
