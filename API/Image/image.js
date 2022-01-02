5008dc5c8f2f5000f0b61fc434bdd035f16cf825
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-02 22:32:39 +0530

    Image API

---
+++ API/Image/image.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import { ImageModel } from "../../Database/allModels";
+
+const Router = express.Router();
+
+/* 
+Route      /
+Des        Uploading the given image to AWS S3 Bucket and then saving the file to mongoDB
+Params     None
+Access     Public
+Method     POST
+*/
+
+Router.post
