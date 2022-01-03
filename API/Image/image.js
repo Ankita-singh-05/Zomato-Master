553cbd747bc0a5ca5e9185d856757766067baf6c
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-03 18:41:30 +0530

    Image api

--- API/Image/image.js
+++ API/Image/image.js
@@ -4,6 +4,8 @@ import multer from "multer";
 
 import { ImageModel } from "../../Database/allModels";
 
+import { s3Upload } from "../../Utils/AWS/S3";
+
 const Router = express.Router();
 
 //Multer CONFIG
@@ -18,4 +20,22 @@ Access     Public
 Method     POST
 */
 
-Router.post
+Router.post("/", upload.single("file"), async (req, res) => {
+    try {
+        const file = req.file;
+        const bucketOptions = {
+            Bucket: "shapeaifullstack123",
+            Key: file.originalname,  // from multer
+            Body: file.buffer,    //from multer
+            ContentType: file.mimetype,  // mimetype -- it is the file type like .img, .png, .svg
+            ACL: "public-read"
+        };
+
+        const uploadImage = await s3Upload(bucketOptions);
+        return res.status(200).json({uploadImage});
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+});
+
+export default Router;
